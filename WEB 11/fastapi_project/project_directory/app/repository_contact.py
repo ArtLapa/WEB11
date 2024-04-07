01efa225f1e93b40d620d3a4e3532653ece807fd
ArtLapa <148677358+ArtLapa@users.noreply.github.com>
2024-04-08 01:12:17 +0300

    Delete WEB 11/fastapi_project/project_directory directory

--- WEB 11/fastapi_project/project_directory/app/repository_contact.py
+++
@@ -1,6 +0,0 @@
-# repository_contact.py
-
-async def get_contact(contact_id: int, db: AsyncSession, current_user: User):
-    stmt = select(Contact).filter_by(id=contact_id, user_id=current_user.id)
-    contact = await db.execute(stmt)
-    return contact.scalar_one_or_none()
